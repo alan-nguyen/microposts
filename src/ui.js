1ac410852742c5a01397070d8b78f053f352ecfe
Alan Nguyen <tuan8101@gmail.com>
2020-05-07 23:22:03 -0600

    Insert cancel button

--- src/ui.js
+++ src/ui.js
@@ -90,6 +90,13 @@ class UI {
       const button = document.createElement('button');
       button.className = 'post-cancel btn btn-light btn-block';
       button.appendChild(document.createTextNode('Cancel Edit'));
+
+      // Get parent
+      const cardForm = document.querySelector('.card-form');
+      // Get element to insert before
+      const formEnd = document.querySelector('.form-end');
+      // Insert cancel button
+      cardForm.insertBefore(button, formEnd);
     } else {
     }
   }
