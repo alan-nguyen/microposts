f446dd55dd5cfccee3237aa5e1507aa5529aaca9
Alan Nguyen <tuan8101@gmail.com>
2020-05-07 22:10:02 -0600

    Fix bugs

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ document.addEventListener('DOMContentLoaded', getPosts);
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
-    .then((data) => ui.showPosts(data)))
+    .then((data) => ui.showPosts(data))
     .catch((err) => console.log(err));
 }
