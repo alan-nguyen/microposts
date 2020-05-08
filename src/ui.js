7128b048d20328cbbb305dfa9fd43e77c9d5c6c5
Alan Nguyen <tuan8101@gmail.com>
2020-05-07 22:45:25 -0600

    Create showAlert method

--- src/ui.js
+++ src/ui.js
@@ -32,7 +32,24 @@ class UI {
     this.post.innerHTML = output;
   }
 
-  clearField() {
+  showAlert(message, className) {
+    this.clearAlert();
+
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    //Get parent
+    const container = document.querySelector('.postsContainer');
+    // Get posts
+    const posts = document.querySelector('#posts');
+    // Insert alert div
+    container.insertBefore(div, posts);
+  }
+
+  clearFields() {
     this.titleInput.value = '';
     this.bodyInput.value = '';
   }
