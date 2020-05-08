80eff56abc2b016a9bf272bc3415bd2306c7efb8
Alan Nguyen <tuan8101@gmail.com>
2020-05-07 22:35:26 -0600

    Create submitPost function

--- src/app.js
+++ src/app.js
@@ -4,12 +4,19 @@ import { ui } from './ui';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
-// Listen for add post
-document.querySelector('.post-submit').addEventListener('click', addPost);
+// Listen for submit post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Get Posts
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
     .then((data) => ui.showPosts(data))
     .catch((err) => console.log(err));
+}
+
+// Submit Post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
 }
